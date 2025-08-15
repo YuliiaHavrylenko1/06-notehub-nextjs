a4c691cab401e252819d93cae209d91ecb6b2cd8
YuliiaHavrylenko1 <gavrilenko.j94@gmail.com>
2025-08-15 19:54:36 +0100

    fixed notes/id/page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,18 +1,22 @@
+import React from 'react';
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
 interface NotePageProps {
-  params: { id: string };
+  params: {
+    id: string;
+  };
 }
 
 export default async function NotePage({ params }: NotePageProps) {
-  const queryClient = new QueryClient();
-  const noteId = params.id;
+  const { id: noteId } = params;
 
   if (!noteId || Array.isArray(noteId)) {
     throw new Error('Invalid note ID');
   }
+
+  const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ['note', noteId],
